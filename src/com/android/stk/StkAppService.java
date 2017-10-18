e47181327ce16d4b195acb48304fa730cd5df2c6
Avinash Nalluri <anallu@codeaurora.org>
2017-10-18 16:20:49 -0700

    Telephony: Display RUN_AT command
    Show the RUN_AT command received
    from the QCRIL via telephony framework
    on the UI.
    
    Change-Id: I64a0668a34bd41753ccba81ba015cd01230a63c0

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -756,6 +756,7 @@ public class StkAppService extends Service implements Runnable {
         switch (cmd.getCmdType()) {
         case SEND_DTMF:
         case SEND_SMS:
+        case RUN_AT:
         case SEND_SS:
         case SEND_USSD:
         case SET_UP_IDLE_MODE_TEXT:
@@ -988,6 +989,7 @@ public class StkAppService extends Service implements Runnable {
             break;
         case SEND_DTMF:
         case SEND_SMS:
+        case RUN_AT:
         case SEND_SS:
         case SEND_USSD:
         case GET_CHANNEL_STATUS:
