0cfe55ad456d4d2e237dad7afa729c0c186764bf
Jiyong Park <jiyong@google.com>
2018-08-14 18:48:20 +0900

    Fix build error
    Add missing import for the Window class.
    
    Test: m -j Stk
    Change-Id: I9ee0210e09c35162a64c45e95ae5a4be37b7e51f

--- src/com/android/stk/StkInputActivity.java
+++ src/com/android/stk/StkInputActivity.java
@@ -33,6 +33,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
